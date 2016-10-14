d9b10fbb9a6c8577993c81b02f545dd0fc174893
obede123321 <obede.silva3@gmail.com>
2016-10-14 14:44:04 -0300

    Acessibilidade e melhorias no site do celle(melhor leitura)

---
+++ SiteCelleCodigoSecreto/principal/php/cabecalho.php
@@ -0,0 +1,156 @@
+<?php
+ob_start();
+session_start();
+?>
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <!-- <title>Sobre</title> -->
+    <meta charset="utf-8">
+    <script src="../javaScript/jquery-1.12.0.min.js"></script>
+    <link rel="shortcut icon" href="../imagens/logoIFPE.png"/>
+    <link rel="stylesheet" type="text/css" href="../css/estilo.css" media="all">
+    <link rel="stylesheet" type="text/css" href="../css/estilo_quiz.css" media="all">
+    <link rel="stylesheet" type="text/css" href="../css/style_cadastro.css" media="all">
+    <link rel="stylesheet" type="text/css" href="../css/kickstart-buttons.css" media="all">
+    <script src="../javaScript/javaScript.js"></script>
+</head>
+<body>
+    <header>
+        <div id="logo">
+            <?php
+
+            // Checa se não existe usuário salvo na sessão (Só vai exibir os campos para login se não existir)
+            if(!isset($_SESSION['usuario'])){
+            ?>
+            <!-- formulário de login -->
+                <div class="login">
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+                        <table id="login" cellspacing="5">
+                            <tr>
+                                <td><span class="log">Usuário</span></td>
+                                <td><input type="text" placeholder="nome" name="usuario" required></td>
+                            </tr>
+                            <tr>
+                                <td><span class="log">Senha</span></td>
+                                <td><input type="password" placeholder="senha" name="senha" required></td>
+                                <td><input class="large" type="submit" name="enviar" Value="Login"></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                </td> 
+                                <td >
+                                <a href="cadastro.php" style="width: 80%;"><small>Cadastre-se aqui</small></a>
+                                </td>   
+                            </tr>
+                            
+                        </table>
+                        
+                    </form>
+                </div>
+                <?php
+            }
+            // Se existir usuário salvo na sessão, exibe uma mensagem de boas vindas e o botão para logout
+            else {
+                ?>
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+                    <input class="submit logout" style="margin-right: 15%; margin-top: 52px;" type="submit" name="logout" value="Logout">
+                </form>
+                <?php
+                echo "<span class='boas_vindas'>Bem vindo(a), <b>" . $_SESSION['usuario'] . "</b>!! </span>";
+            }
+            // Se o botão para 'logout' for clicado, tira o usuário da sessão e redireciona para página home.php
+            if(isset($_POST['logout'])) {
+                unset($_SESSION['usuario']);
+                header("location: home.php");
+            }
+            ?>
+            <div class="banner">
+                <div id="logo-reitoria">
+                    <img src="../imagens/logo.png" widht="300px" height="90px">
+                    <a class="acesso-reitoria"></a>
+                </div>
+            </div>
+        </div>  
+        <nav id="menu">
+            <ul class="menu">        
+                <li><a href="home.php" class="botaoMenu" id="home.php">Home</a></li>
+                <li><a href="sobre.php" class="botaoMenu" id="sobre.php">Sobre</a></li>
+                <li><a href="noticias.php" class="botaoMenu" id="noticias.php">Notícias</a></li>
+                <li><a href="#" id="#">Mídia</a>
+                    <ul>
+                        <li><a href="videos.php" class="botaoMenu" id="videos.php">Vídeos</a></li>
+                        <li><a href="audios.php" class="botaoMenu" id="audios.php">Áudios</a></li>
+                    </ul>
+                </li>
+                <li><a href="#" id="#">Atividades</a>
+                    <ul>
+        <li><a href="quiz.php" class="botaoMenu">Quiz 1</a></li>
+        <li><a href="quiz_2.php" class="botaoMenu">Quiz 2</a></li>
+                    </ul>
+                </li>
+                <li><a href="#" id="#">Conteúdos</a>
+                    <ul>
+                        <li><a href="paises_nacionalidades.php" class="botaoMenu" id="paises_nacionalidades.php">Países e Nacionalidades</a></li>
+                        <li><a href="expressoes_termos.php" class="botaoMenu" id="expressoes_termos.php">Expressões e Termos</a></li>
+            <li><a href="numerais.php" class="botaoMenu" id="numerais.php">Numerais</a></li>
+    <li><a href="medidas.php" class="botaoMenu" id="medidas.php">Medidas</a></li>
+                    </ul>    
+                </li>
+                <li><a href="arquivos.php" class="botaoMenu" id="arquivos.php">Envio</a></li>
+                <li><a href="sites_relacionados.php" class="botaoMenu" id="sites_relacionados.php">Sites Relacionados</a></li>        
+                <li><a href="contatos.php" class="botaoMenu" id="CONTATOS.php">Contato</a></li>
+            </ul>
+            <br><br>
+        </nav>
+    </header>
+    <?php
+
+    // validação do usuário para login
+
+    if(isset($_POST['enviar'])) {
+        $user = $_POST['usuario'];      // Salva o conteúdo do input name="usuario" do form de login na variável $user
+        $pass = md5($_POST['senha']);   // Salva o conteúdo do input name="senha" do form de login na variável $pass
+        
+        // Salva os dados do servidor, usuario, senha e nome do banco de dados para fazer a conexão
+
+        $server = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "site_celle";
+
+        $link = mysqli_connect ($server, $username, $password, $dbname);    // Faz a conexão com o banco de dados
+        
+        // consulta para puxar do banco usuario e senha que sejam iguais aos digitados no form de login
+
+        $query = "SELECT senha, usuario FROM usuarios WHERE usuario='$user' AND senha='$pass'";
+        $result = mysqli_query($link, $query);      // Executa a query e salva o resultado
+        
+        // Checa se houve resultado da consulta
+        if($result){
+            $arr = mysqli_fetch_assoc($result);     // Transforma o resultado da consulta em um array associativo (os índices são as colunas da tabela 'usuarios')
+            $row = mysqli_num_rows($result);        // Recebe o número de linhas retornadas da consulta ao banco
+
+            // Checa se o usuário e senha digitados no form de login são iguais aos que estão salvos no banco de dados
+            if($user == $arr['usuario'] && $pass == $arr['senha']){
+                // Se verdadeiro, salva o usuário na sessão
+                $_SESSION['usuario'] = $user;
+            }
+
+            // Checa se o número de linhas retornadas da consulta é diferente de zero. Se verdadeiro, redireciona para a página "home.php"
+            if($row != 0) {
+                header("location: home.php");
+            }
+
+            // Senão, exibe um alerta para o usuário
+            else {
+                echo "<script>
+                alert('Usuário ou senha incorretos!!');
+                </script>";
+            }
+        }
+        // Encerra a conexão com o banco de dados
+        mysqli_close($link);
+    }
+    ob_end_flush();
+    ?>
---
+++ SiteCelleCodigoSecreto/principal/php/home.php
@@ -0,0 +1,152 @@
+<?php 
+	include_once 'cabecalho.php';
+?>
+	<script type="text/javaScript" src="../javaScript/jquery.js"></script>
+    <script type="text/javaScript" src="../javaScript/script.js"></script>
+ <div id="home" class="container-fluid">   
+<title>Home</title>
+<section>
+	<div id="conteudo">
+		<h1 class="t1">CELLE</h1> 
+		<hr id="hr-top"></hr>
+		<p> O domínio básico de ao menos um idioma além do Português é um dos diferenciais que elevam a possibilidade de contratação e os perfis dos cursos ofertados pelo campus Igarassu (Informática Para Internet e Logística) motivaram o CELLE a ofertar o ensino da língua inglesa. Conscientes disso, 37 discentes se inscreveram no processo seletivo, que não teve cobrança de taxas, mas previa a entrega voluntária de um quilo de alimento não perecível ou um kit de higiene pessoal.
+	    As aulas serão realizadas nas quartas e quintas, das 12h50 às 14h20, no próprio campus, que funciona na Faculdade de Ciências Humanas e Sociais de Igarassu (FACIG), até que a sede própria seja construída. Antes da primeira aula, o coordenador do CELLE, Ivo Felix, acompanhado das alunas Ellaine Dayane e Vanessa Clarissa, realizou a entrega dos donativos arrecadados para o Orfanato Santo Antônio.</p>
+		<figure id="slide"> 
+            <img src="../Imagens/fotocelle/2.JPG" alt="Alunos participam da 1ª aula do CELLE Igarassu">
+            <img src="../Imagens/fotocelle/4.JPG" alt="Alunos participam da 1ª aula do CELLE Igarassu">
+            <img src="../Imagens/fotocelle/5.JPG" alt="Alunos participam da 1ª aula do CELLE Igarassu">
+        </figure> 
+	</div>	
+</section>
+</div>
+<?php 
+	include_once 'rodape.php';
+?>
+<?php 
+	include_once 'cabecalho.php';
+?>
+<div id="sobre" class="container-fluid">
+<title>Sobre</title>
+<section>
+    <div id="conteudo">
+        <h1 class="t1">Sobre</h1>
+        <hr id="hr-top"></hr>
+        <p style="text-align: justify;">O CELLE é um espaço destinado à promoção de oportunidade de aquisição de conhecimentos em outras línguas e participação em atividades culturais inerentes à internacionalização para os discentes, servidores, pesquisadores e comunidade do IFPE.</p><br>
+        <p style="text-align: justify;">O CELLE tem por Objetivo Geral oferecer aos servidores, aos discentes e à comunidade externa o ensino de uma ou mais línguas adicionais, visando à mobilidade acadêmica e à cooperação internacional (transferência de tecnologia, pesquisa, produção acadêmica, mobilidade acadêmica, redação e tradução de documentos internos, entre outros).</p><br> 
+        <p style="text-align: justify; text-indent: 0;">O CELLE tem por Objetivos Específicos: <br><br>
+        I - Ofertar cursos de Língua Inglesa, Língua Espanhola e Língua Francesa .
+        <br>
+        II - Ofertar curso de Língua Portuguesa e Cultura Brasileira para Estrangeiros, na modalidade presencial.
+        <br>
+        III - Ofertar cursos de LIBRAS.
+        <br>
+        IV - Desenvolver pesquisas aplicadas ao ensino-aprendizagem de idiomas.
+        <br>
+        V - Aplicar testes de proficiência para estudantes de pós-graduação, pesquisadores, servidores e discentes interessados na mobilidade internacional do IFPE.
+         <br>
+        VI - Elaborar e aplicar testes de nivelamento de proficiência em âmbito interno.
+         <br>
+        VII - Preparar professores de línguas para serem aplicadores de testes de proficiência internacionais e para ministrar cursos preparatórios para os referidos testes, como centro reconhecido.
+         <br>
+        VIII - Ofertar seminários e cursos de aperfeiçoamento na área de ensino/aprendizagem de línguas.
+         <br>
+        IX - Oferecer material de apoio para servidores e estudantes que desejam realizar estudos individuais nos laboratórios multimídia.
+         <br>
+        X- Preparar servidores para ministrar cursos para fins específicos.
+         <br>
+        XI - Oferecer cursos para fins específicos de acordo com as demandas das comunidades interna e externa. </p><br>
+        <p style="text-align: justify;">O CELLE é constituído de um Colegiado, que é um órgão democrático e participativo de função deliberativa, consultiva, propositiva e de planejamento acadêmico dos cursos de Línguas do IFPE, representado por duas instâncias:
+        <br><br>
+        I – Uma Comissão Sistêmica composta pelos seguintes membros:
+        <br>
+        - Coordenador Geral do CELLE;
+        <br>
+        - Vice-coordenador Geral do CELLE;
+        <br>
+        - Os Coordenadores Gerais Setoriais (representantes por campus);
+        <br>
+        - Os Vice-coordenadores Gerais Setoriais (em condição de suplência, representantes por campus);
+        <br>
+        <br>
+        II Uma Comissão Setorial do CELLE Local:
+        <br>
+        - Coordenador Geral Setorial; <br>
+        - Vice-Coordenador Geral Setorial; <br>
+        - Técnico-administrativo; <br>
+        - Docentes atuantes no CELLE; <br>
+        - Representação estudantil</p>
+  
+  </div>
+    <br/>
+</section>
+</div>
+<?php 
+	include_once 'rodape.php';
+?>
+<?php 
+	include_once 'cabecalho.php';
+?>
+<div id="noticias" class="container-fluid">
+<title>Notícias</title>
+<section>
+	<div id="conteudo">
+	<h1 class="t1"> Notícias</h1>
+	<hr id="hr-top"></hr>
+	    <h2>Alunos participam da 1ª aula do CELLE Igarassu</h2>
+	      <p> Na última quarta-feira (7/10), 18 alunos do Campus Igarassu iniciaram as atividades do Centro de Línguas Estrangeiras (CELLE), oferecido pelo IFPE como parte das ações e projetos para garantir empregabilidade. Os estudantes, que estão matriculados no 2º período dos cursos técnicos da modalidade subsequente, foram selecionados para o curso de Inglês Comunicativo, através de seleção que avaliou as maiores médias gerais do 1º período. O processo seletivo rendeu frutos na forma de donativos para o Orfanato Santo Antônio, que dá assistência para cerca de cem crianças e adolescentes do município...<a href="noticia_1aula.php"><strong>Leia mais</strong></a></p>
+	      <h2>Docentes do IFPE selecionados para capacitação nos Estados Unidos</h2>
+	      <p> Nove professores de língua inglesa do IFPE foram habilitados no resultado final da Chamada Pública do Programa SETEC-CAPES/NOVA, iniciativa que levará professores deste idioma que atuam em instituições da Rede Federal de Educação Profissional, Científica e Tecnológica para passarem por capacitação nos Estados Unidos. No total, a Chamada contemplou 75 professores, de diferentes instituições...<a href="noticia_viagem.php"><strong>Leia mais</strong></a></p>
+	</div>
+</section>
+</div>
+<?php 
+	include_once 'rodape.php';
+?>
+<?php 
+	include_once 'cabecalho.php';
+?>
+<div id="sites_relacionados" class="container-fluid">
+<title>Sites Relacionados</title>
+<section>
+	<div id="conteudo">
+		<h1 class="t1">Sites Relacionados</h1>
+		<hr id="hr-top"></hr>
+		<ul>
+			<li style="list-style-type:none; line-height: 25px;"><a href="http://www.agendaweb.org/vocabulary/house_furniture-exercises.html" target="_blank">Agenda Web - Vocabulary House Furniture - Exercises</a></li>
+			<li style="list-style-type:none; line-height: 25px;"><a href="http://www.agendaweb.org/grammar/there_is_are-exercises.html" target="_blank">Agenda Web - Grammar There is are - Exercises</a></li>
+			<li style="list-style-type:none; line-height: 25px;"><a href="http://www.eclecticenglish.com/grammar/PresentContinuous1F.html" target="_blank">Eclectic English - Grammar Present Continuous - Exercises</a></li>
+			<li style="list-style-type:none; line-height: 25px;"><a href="http://www.really-learn-english.com/simple-present-exercises.html" target="_blank">Really Learn English - Simple Present - Exercises</a></li>
+			<li style="list-style-type:none; line-height: 25px;"><a href="http://www.agendaweb.org/verbs/to-be-exercises.html" target="_blank">Agenda Web - Verbs/to be - Exercises</a></li>
+		</ul>
+	</div>
+</section>
+</div>
+<?php 
+	include_once 'rodape.php';
+?>
+<?php 
+	include_once 'cabecalho.php';
+?>
+<div id="contatos" class="container-fluid">
+<title>Contatos</title>
+<section>
+	<div id="conteudo">
+		<h1 class="t1">Contato</h1>
+		<hr id="hr-top"></hr>
+		<p style="text-indent: 0;">
+		    Coordenador do CELLE - IFPE (Campus Igarassu):
+		    <br>Professor Ivo Felix Gualberto de Sá
+		   	<br>celle@igarassu.ifpe.edu.br
+		    <br>Avenida Alfredo Bandeira de Melo S/N, BR-101 Norte, Km 44, Igarassu-PE
+		    <br>
+		    
+		    <br>Coordenadora-geral do CELLE - IFPE:
+		    <br>Professora Rita Fabiana
+		    <br>celle@reitoria.ifpe.edu.br
+		</p>
+	</div>
+</section>
+</div>
+<?php 
+	include_once 'rodape.php';
+?>
